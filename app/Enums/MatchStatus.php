63b9aa71495d424c13bb29399227ac790ed226c6
Alexis79Bck <alexis.ed.mata@gmail.com>
2026-06-09 11:00:40 -0400

    Feat: agregar nuevos estados y atributos a la tabla de juegos en la migración

--- app/Enums/MatchStatus.php
+++ app/Enums/MatchStatus.php
@@ -7,6 +7,11 @@ enum MatchStatus: string
     case PROGRAMADO = 'Programado';
     case EN_PROGRESO = 'En Progreso';
     case FINALIZADO = 'Finalizado';
+    case SUSPENDIDO = 'Suspendido';
+    case APLAZADO = 'Aplazado';
+    case CANCELADO = 'Cancelado';
+    case RETRASADO = 'Retrasado';
+    case ANULADO = 'Anulado';
 
     public function getColor(): string
     {
@@ -14,6 +19,11 @@ enum MatchStatus: string
             self::PROGRAMADO => '#3b82f6', // Azul
             self::EN_PROGRESO => '#f59e0b', // Naranja
             self::FINALIZADO => '#10b981', // Verde
+            self::SUSPENDIDO => '#ef4444', // Rojo
+            self::APLAZADO => '#8b5cf6', // Morado
+            self::CANCELADO => '#6b7280', // Gris
+            self::RETRASADO => '#984102', // Marrón
+            self::ANULADO => '#100b0b', // Negro
         };
     }
 
--- database/migrations/2026_04_14_000005_create_matches_table.php
+++ database/migrations/2026_04_14_000005_create_matches_table.php
@@ -12,15 +12,41 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('juegos', function (Blueprint $table) {
-            $table->id();
+            $table->id(); //Primary Key de la tabla
+
+            $table->unsignedBigInteger('external_id')->unique(); //ID externo del juego, único para cada juego. Relacionado al Proveedor de datos (e.g., football-data.org)
+            $table->string('proveedor'); //Proveedor de datos (e.g., football-data.org)
+
+            // Relaciones con otras tablas
+            $table->foreignId('temporada_id')->constrained('temporadas')->onDelete('cascade');
             $table->foreignId('etapa_id')->constrained('etapas')->onDelete('cascade');
+            $table->foreignId('grupo_id')->nullable()->constrained('grupos')->nullOnDelete();
+
+            // Atributos de esta tabla
             $table->foreignId('equipo_local_id')->constrained('equipos')->onDelete('cascade');
             $table->foreignId('equipo_visitante_id')->constrained('equipos')->onDelete('cascade');
-            $table->datetime('fecha_hora');
+            $table->unsignedSmallInteger('jornada')->nullable();
+            $table->dateTime('fecha_hora');
+            $table->dateTime('fecha_cierre_predicciones');
+
+            // Equipo ganador (si el juego ha finalizado)
+            $table->foreignId('equipo_ganador_id')->nullable()->constrained('equipos')->nullOnDelete();
+
+            // Resultado en Tiempo Reglamentario
             $table->unsignedInteger('equipo_local_goles')->default(0);
             $table->unsignedInteger('equipo_visitante_goles')->default(0);
+
+            // Resultado en Prorroga (si aplica)
+            $table->unsignedInteger('equipo_local_goles_prorroga')->nullable();
+            $table->unsignedInteger('equipo_visitante_goles_prorroga')->nullable();
+
+            // Resultado en Penales (si aplica)
+            $table->unsignedInteger('equipo_local_goles_penales')->nullable();
+            $table->unsignedInteger('equipo_visitante_goles_penales')->nullable();
+
+            // Estado del juego
             $table->string('estado')->default('programado'); // programado, en_progreso, finalizado
-            // $table->enum('estado', ['programado', 'en_progreso', 'finalizado'])->default('programado');
+
             $table->timestamps();
 
             // Índices para mejor rendimiento
